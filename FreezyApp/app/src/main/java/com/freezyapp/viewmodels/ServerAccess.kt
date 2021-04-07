c50066dc530c1f49e8f77401a66dd8e1943baa5e
AntiElysium <patrick@pilsby.dk>
2021-04-07 16:18:40 +0200

    Update ServerAccess.kt

--- FreezyApp/app/src/main/java/com/freezyapp/viewmodels/ServerAccess.kt
+++ FreezyApp/app/src/main/java/com/freezyapp/viewmodels/ServerAccess.kt
@@ -3,6 +3,7 @@ package com.freezyapp.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -41,39 +42,39 @@ interface AuthenticationService {
 
 interface CategoryService {
     @POST("/Create/Category")
-    fun createCategory(accessToken: UUID, name: String, color: String): Call<CategoryData> //Måske unødvendigt med return type
+    fun createCategory(accessToken: UUID, name: String, color: String): Call<ResponseBody> //Måske unødvendigt med return type
 
     @GET("/AllCategories")
     fun getAllCategories(accessToken: UUID): Call<List<CategoryData>>
 
     @PUT("/Update/Category")
-    fun updateCategory(accessToken: UUID, categoryId: Long, color: String) //Maybe just like this????
+    fun updateCategory(accessToken: UUID, categoryId: Long, color: String): Call<ResponseBody> //Maybe just like this????
 
     @DELETE("/Delete/Category")
-    fun deleteCategory(accessToken: UUID, categoryId: Long)
+    fun deleteCategory(accessToken: UUID, categoryId: Long): Call<ResponseBody>
 }
 
 interface ItemService {
     @POST("/Create/Item")
-    fun createItem(accessToken: UUID, name: String, expirationDate: LocalDateTime, unit: String, storage_Unit_Id: Long, categoryIds: List<Long>): Call<ItemData> //Muligvis ikke ItemData i Call
+    fun createItem(accessToken: UUID, name: String, expirationDate: LocalDateTime, unit: String, storage_Unit_Id: Long, categoryIds: List<Long>): Call<ResponseBody>
 
     @GET("/Items")
     fun getAllItems(accessToken: UUID, storageId: Long): Call<List<ItemData>>
 
     @DELETE("/Delete/Item")
-    fun deleteItem(accessToken: UUID, itemId: Long): Call<ItemData> //Muligvis ikke ItemData i Call
+    fun deleteItem(accessToken: UUID, itemId: Long): Call<ResponseBody>
 }
 
 interface StorageService {
     @POST("/Create/Storage")
-    fun createStorage_Unit(accessToken: UUID, name: String): Call<StorageData>
+    fun createStorage_Unit(accessToken: UUID, name: String): Call<ResponseBody>
 
     @GET("/AllStorages")
     fun getAllStorages(accessToken: UUID): Call<List<StorageData>>
 
     @PUT("/Update/Storage")
-    fun updateStorage_Unit(accessToken: UUID, storageId: Long/*, updateInfo: ????*/): Call<StorageData>
+    fun updateStorage_Unit(accessToken: UUID, storageId: Long/*, updateInfo: ????*/): Call<ResponseBody>
 
     @DELETE("/Delete/Storage")
-    fun deleteStorage_Unit(accessToken: UUID, name: String): Call<StorageData>
+    fun deleteStorage_Unit(accessToken: UUID, name: String): Call<ResponseBody>
 }
