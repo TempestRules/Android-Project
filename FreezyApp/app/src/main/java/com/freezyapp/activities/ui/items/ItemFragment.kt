e26e2b10da60cf9358578e0317026b9fca040b8e
Denmads <43139465+Denmads@users.noreply.github.com>
2021-04-08 15:05:49 +0200

    Start drawer navigation

---
+++ FreezyApp/app/src/main/java/com/freezyapp/activities/ui/items/ItemFragment.kt
@@ -0,0 +1,21 @@
+package com.freezyapp.activities.ui.items
+
+import android.os.Bundle
+import android.os.Debug
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.freezyapp.R
+
+class ItemFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_item, container, false)
+    }
+}
