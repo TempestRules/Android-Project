9b8b97d21bc861cefeb5a9400b4c6741f76d2bb5
AntiElysium <patrick@pilsby.dk>
2021-04-21 14:36:37 +0200

    StorageModel

--- FreezyApp/app/src/main/java/com/freezyapp/viewmodels/StorageModel.kt
+++ FreezyApp/app/src/main/java/com/freezyapp/viewmodels/StorageModel.kt
@@ -1,7 +1,10 @@
 package com.freezyapp.viewmodels
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.freezyapp.viewmodels.entities.Category
 import com.freezyapp.viewmodels.entities.Storage_Unit
 import com.freezyapp.viewmodels.requestbodies.StorageData
 import okhttp3.ResponseBody
@@ -16,6 +19,17 @@ import java.util.*
 class StorageModel : ViewModel() {
     val BASE_URL = "http://10.0.2.2:8080"
     private var retrofit: Retrofit? = null
+    var mld = MutableLiveData<List<Storage_Unit>>()
+    var liveList: LiveData<List<Storage_Unit>> = mld
+    private var currentStorage_Unit: Storage_Unit? = null
+
+    fun getCurrentStorageUnit(): Storage_Unit? {
+        return currentStorage_Unit
+    }
+
+    fun setCurrentStorageUnit(storageUnit: Storage_Unit?){
+        currentStorage_Unit = storageUnit
+    }
 
     fun getClient(): Retrofit {
         if(retrofit == null){
@@ -37,13 +51,13 @@ class StorageModel : ViewModel() {
             override fun onResponse(call: Call<Void>, response: Response<Void>) {
                 if(response != null){
                     if(response.code() == 201){
-                        Log.d("uSto","Updated storage unit")
+                        Log.d("cSto","Updated storage unit")
                     }
                 }
             }
 
             override fun onFailure(call: Call<Void>, t: Throwable) {
-                Log.d("uStoError","Error in updating storage unit: " + t.message)
+                Log.d("cStoError","Error in updating storage unit: " + t.message)
             }
 
         })
@@ -58,13 +72,57 @@ class StorageModel : ViewModel() {
             override fun onResponse(call: Call<List<Storage_Unit>>, response: Response<List<Storage_Unit>>) {
                 if(response != null){
                     if(response.code() == 200) {
-
+                        mld.value = response.body()
+                        Log.d("gaCat","Successfully read all storage units")
                     }
                 }
             }
 
             override fun onFailure(call: Call<List<Storage_Unit>>, t: Throwable) {
-                Log.d("")
+                Log.d("gaStoError", "Error in getting all storage units: " + t.message)
+            }
+
+        })
+    }
+
+    fun updateStorage(accessToken: UUID, storageId: Long, updateInfo: Storage_Unit){
+        val service = getClient().create(StorageService::class.java)
+        var sd = StorageData()
+        sd.setAccessToken(accessToken)
+        sd.setStorageId(storageId)
+        sd.setUpdateInfo(updateInfo)
+        val call = service.updateStorage_Unit(sd)
+        return call.enqueue(object: Callback<Void> {
+            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                if(response != null){
+                    if(response.code() == 200){
+                        Log.d("uSto", "Updated storage unit")
+                    }
+                }
+            }
+            override fun onFailure(call: Call<Void>, t: Throwable) {
+                Log.d("uSto","Error in updating storage unit: " + t.message)
+            }
+        })
+    }
+
+    fun deleteStorage(accessToken: UUID, name: String){
+        val service = getClient().create(StorageService::class.java)
+        var sd = StorageData()
+        sd.setAccessToken(accessToken)
+        sd.setName(name)
+        val call = service.deleteStorage_Unit(sd)
+        return call.enqueue(object: Callback<Void> {
+            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                if(response != null){
+                    if(response.code() == 200){
+                        Log.d("dSto", "Deleted storage unit")
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<Void>, t: Throwable) {
+                Log.d("dStoError","Error in deleting storage unit: " + t.message)
             }
 
         })
@@ -79,8 +137,8 @@ interface StorageService {
     fun getAllStorages(@Body storageData: StorageData/*accessToken: UUID*/): Call<List<Storage_Unit>>
 
     @PUT("/Update/Storage")
-    fun updateStorage_Unit(@Body storageData: StorageData/*accessToken: UUID, storageId: Long, updateInfo: ????*/): Call<ResponseBody>
+    fun updateStorage_Unit(@Body storageData: StorageData/*accessToken: UUID, storageId: Long, updateInfo: ????*/): Call<Void>
 
     @DELETE("/Delete/Storage")
-    fun deleteStorage_Unit(@Body storageData: StorageData/*accessToken: UUID, name: String*/): Call<ResponseBody>
+    fun deleteStorage_Unit(@Body storageData: StorageData/*accessToken: UUID, name: String*/): Call<Void>
 }
