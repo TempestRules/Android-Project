d4fdc9f808e3404186c824c69db6ba85f1395c55
Denmads <43139465+Denmads@users.noreply.github.com>
2021-04-27 09:05:59 +0200

    Layouts for category section

--- FreezyApp/app/src/main/java/com/freezyapp/activities/ui/storage/StorageFragment.kt
+++ FreezyApp/app/src/main/java/com/freezyapp/activities/ui/storage/StorageFragment.kt
@@ -1,15 +1,11 @@
 package com.freezyapp.activities.ui.storage
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentContainer
 import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.commit
 import com.freezyapp.R
-import com.freezyapp.activities.ui.storage.fragements.StorageFormFragment
 import com.freezyapp.activities.ui.storage.fragements.StorageListFragment
 
 class StorageFragment : Fragment(R.layout.fragment_storage) {
