83760bfcd007eb6bfec981dcc48a25acd9d6d28a
Denmads <43139465+Denmads@users.noreply.github.com>
2021-05-10 10:23:47 +0200

    Fixed AccessToken class

--- FreezyApp/app/src/main/java/com/freezyapp/backend/AccessToken.kt
+++ FreezyApp/app/src/main/java/com/freezyapp/backend/AccessToken.kt
@@ -3,6 +3,7 @@ package com.freezyapp.backend
 import android.content.Context
 import android.content.SharedPreferences
 import com.freezyapp.R
+import java.lang.Error
 import java.util.*
 
 class AccessToken() {
@@ -12,15 +13,19 @@ class AccessToken() {
 
         fun setContext(context: Context) {
             AccessToken.context = context
+            pref = Companion.context.getSharedPreferences(file_key, Context.MODE_PRIVATE)
         }
 
         private const val file_key: String = "AccessTokenFile"
         private const val token_key: String = "AccessToken"
 
-        private val pref: SharedPreferences = context.getSharedPreferences(file_key, Context.MODE_PRIVATE)
+        private var pref: SharedPreferences? = null
 
         fun get(): UUID? {
-            val storedValue = pref.getString(token_key, null)
+            if (pref == null)
+                return null
+
+            val storedValue = pref!!.getString(token_key, null)
             return if (storedValue == null) {
                 null
             } else {
@@ -29,7 +34,11 @@ class AccessToken() {
         }
 
         fun set(accessToken: UUID) {
-            with (pref.edit()) {
+            if (pref == null) {
+                throw Error("Context have not been sat!");
+            }
+
+            with (pref!!.edit()) {
                 putString(token_key, accessToken.toString())
                 apply()
             }
